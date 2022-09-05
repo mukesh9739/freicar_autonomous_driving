0e6e6cadca7f8536a1a066d5dc285bd4935bb942
mukesh9739 <mukesh9739@gmail.com>
2022-09-05 17:02:57 +0200

    Delete ros_code directory

--- ros_code/freicar_launch_/bash/commands/straight.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rostopic pub -1 /freicar_commands freicar_common/FreiCarControl  '{name:  "greatteam", command: "straight"}'
--- ros_code/freicar_launch_/bash/record.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rosbag record /tf /tf_static /freicar_1/sim/camera/depth/front/image_float /freicar_1/sim/camera/rgb/front/image /freicar_1/odometry
